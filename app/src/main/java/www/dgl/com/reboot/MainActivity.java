ce9dbd8220736e270bc0658b751e6834119b5474
dugaolong <1872387570@qq.com>
2017-07-14 15:19:39 +0800

    init

---
+++ app/src/main/java/www/dgl/com/reboot/MainActivity.java
@@ -0,0 +1,29 @@
+package www.dgl.com.reboot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"reboot",Toast.LENGTH_LONG).show();
+                Intent intent2 = new Intent(Intent.ACTION_REBOOT);
+                intent2.putExtra("nowait", 1);
+                intent2.putExtra("interval", 1);
+                intent2.putExtra("window", 0);
+                sendBroadcast(intent2);
+            }
+        });
+    }
+}
